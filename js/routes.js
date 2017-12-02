768764c2d1679a8a79eb708b6c4dd5c34c740e12
Vladimir <vdem@in-syst.ru>
2017-12-03 00:39:03 +0300

    Create routes.js

---
+++ js/routes.js
@@ -0,0 +1,89 @@
+var routes = [
+  // Index page
+  {
+    path: '/',
+    url: './index.html',
+    name: 'home',
+  },
+  // About page
+  {
+    path: '/about/',
+    url: './pages/about.html',
+    name: 'about',
+  },
+  // Right Panel pages
+  {
+    path: '/panel-right-1/',
+    content: '\
+      <div class="page">\
+        <div class="navbar">\
+          <div class="navbar-inner sliding">\
+            <div class="left">\
+              <a href="#" class="link back">\
+                <i class="icon icon-back"></i>\
+                <span class="ios-only">Back</span>\
+              </a>\
+            </div>\
+            <div class="title">Panel Page 1</div>\
+          </div>\
+        </div>\
+        <div class="page-content">\
+          <div class="block">\
+            <p>This is a right panel page 1</p>\
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo saepe aspernatur inventore dolorum voluptates consequatur tempore ipsum! Quia, incidunt, aliquam sit veritatis nisi aliquid porro similique ipsa mollitia eaque ex!</p>\
+          </div>\
+        </div>\
+      </div>\
+    ',
+  },
+  {
+    path: '/panel-right-2/',
+    content: '\
+      <div class="page">\
+        <div class="navbar">\
+          <div class="navbar-inner sliding">\
+            <div class="left">\
+              <a href="#" class="link back">\
+                <i class="icon icon-back"></i>\
+                <span class="ios-only">Back</span>\
+              </a>\
+            </div>\
+            <div class="title">Panel Page 2</div>\
+          </div>\
+        </div>\
+        <div class="page-content">\
+          <div class="block">\
+            <p>This is a right panel page 2</p>\
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo saepe aspernatur inventore dolorum voluptates consequatur tempore ipsum! Quia, incidunt, aliquam sit veritatis nisi aliquid porro similique ipsa mollitia eaque ex!</p>\
+          </div>\
+        </div>\
+      </div>\
+    ',
+  },
+
+  // Components
+  {
+    path: '/accordion/',
+    url: './pages/accordion.html',
+  },
+  {
+    path: '/action-sheet/',
+    componentUrl: './pages/action-sheet.html',
+  },
+  {
+    path: '/autocomplete/',
+    componentUrl: './pages/autocomplete.html',
+  },
+  {
+    path: '/badge/',
+    componentUrl: './pages/badge.html',
+  },
+  {
+    path: '/buttons/',
+    url: './pages/buttons.html',
+  },
+  {
+    path: '/calendar/',
+    componentUrl: './pages/calendar.html',
+  }
+  ];
