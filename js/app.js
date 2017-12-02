a55eb5311875c5f54d8f910b93af1a7b1ef95019
Vladimir <vdem@in-syst.ru>
2017-12-03 01:38:23 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,17 +1,10 @@
 // Dom7
 var $ = Dom7;
-
-// Theme
-var theme = 'auto';
-if (document.location.search.indexOf('theme=') >= 0) {
-  theme = document.location.search.split('theme=')[1].split('&')[0];
-}
-
 // Init App
 var app = new Framework7({
   id: 'io.framework7.testapp',
   root: '#app',
-  theme: theme,
+  theme: 'auto',
   data: function () {
     return {
       user: {
@@ -30,3 +23,28 @@ var app = new Framework7({
     placementId: 'pltd4o7ibb9rc653x14',
   },
 });
+
+
+function GoogleMap(){
+	this.initialize = function(){
+		var map = showMap();
+	}
+
+	var showMap = function(){
+		var mapOptions = {
+		zoom: 9,
+		center: new google.maps.LatLng(41.0669484, 29.0122383),
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+		}
+		var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+
+		return map;
+	}
+}
+
+function initMap(){
+	var map = new GoogleMap();
+	map.initialize();
+	console.log('init');
+}
+
