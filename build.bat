88b007a62167f189f3eeeb57b5f44ad0595b537b
Vladimir-ПК\Vladimir <vdem@in-syst.ru>
2017-12-10 23:47:30 +0300

    Auto Commit

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
+Path=C:\Program Files\Git\bin;F:\git\curl-7.57.0\AMD64
+
 git pull
 
 git add -A
@@ -6,4 +8,11 @@ git commit -a -m "Auto Commit"
 
 git push
 
-@set /P var="finish"
+# token = ToQ9uvUPmLpH6M_zzXZD
+# app = 2912599
+# ?auth_token=ASTRINGTOKEN
+
+curl -X POST -d '' https://build.phonegap.com/api/v1/apps/2912599/build/android?auth_token=ToQ9uvUPmLpH6M_zzXZD
+
+
+set /P var="finish"
