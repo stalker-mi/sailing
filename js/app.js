7cbeca58df9fe881a3e529c7489fc9e3f0cc82d6
Vladimir-ПК\Vladimir <vdem@in-syst.ru>
2017-12-23 23:25:49 +0300

    Auto Commit

--- js/app.js
+++ js/app.js
@@ -48,3 +48,74 @@ function initMap(){
 	map.initialize();
 }
 
+
+var db = window.openDatabase("Dummy_DB", "1.0", "Just a Dummy DB", 200000); //will create database Dummy_DB or open it
+db.transaction(populateDB, errorCB, successCB);
+
+function populateDB(tx) {
+	tx.executeSql('CREATE TABLE IF NOT EXISTS boats (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, number TEXT, len INTEGER)');
+	//tx.executeSql('INSERT INTO SoccerPlayer(Name,Club) VALUES ("Alexandre Pato", "AC Milan")');
+	//tx.executeSql('INSERT INTO SoccerPlayer(Name,Club) VALUES ("Van Persie", "Arsenal")');
+}
+ 
+    //function will be called when an error occurred
+    function errorCB(err) {
+        console.log("Error processing SQL: "+err.code);
+    }
+ 
+    //function will be called when process succeed
+    function successCB() {
+        db.transaction(queryDB,errorCB);
+    }
+ 
+    //select all from SoccerPlayer
+    function queryDB(tx){
+        tx.executeSql('SELECT * FROM boats',[],querySuccess,errorCB);
+    }
+    
+    function querySuccess(tx,result){
+		//console.log(result);
+	}
+
+$(document).on('click', '.back_to_boats', function (e) {
+	if($('#new_boat_name').val() == ""){
+		app.notification.create({
+			title: 'Error',
+			text: 'Please provide boat name',
+			closeTimeout: 2000
+		}).open();
+	}
+	else{
+		save_boat();
+	}
+});
+
+
+function save_boat(){
+	db.transaction(
+		function(tx){
+			tx.executeSql('INSERT INTO boats(name,number,len) VALUES ("'+$('#new_boat_name').val()+'", "'+$('.sail_number').val()+'", "'+$('.boat_length').val()+'")');
+		},
+		errorCB);
+	get_boats(function(){
+		//app.router.back()
+		app.router.navigate('/boats/');
+	});
+}
+
+// get all boats
+var boats = [];
+get_boats();
+function get_boats(callback){
+	boats = [];
+	db.transaction(function(tx){
+		tx.executeSql('SELECT * FROM boats',[],function(tx,results){
+			for (var i=0; i<results.rows.length; i++){
+				boats.push(results.rows.item(i));
+			}
+			if(callback){
+				callback();
+			}
+		 },errorCB);
+	 },errorCB);
+}
--- js/routes.js
+++ js/routes.js
@@ -7,59 +7,16 @@ var routes = [
   },
   // About page
   {
-    path: '/about/',
-    url: './pages/about.html',
-    name: 'about',
+    path: '/boats/',
+    componentUrl: './pages/boats.html',
+    name: 'boats',
+  },
+  {
+    path: '/boats/add_boat/',
+    url: './pages/add_boat.html',
+    name: 'add_boat',
   },
   // Right Panel pages
-  {
-    path: '/panel-right-1/',
-    content: '\
-      <div class="page">\
-        <div class="navbar">\
-          <div class="navbar-inner sliding">\
-            <div class="left">\
-              <a href="#" class="link back">\
-                <i class="icon icon-back"></i>\
-                <span class="ios-only">Back</span>\
-              </a>\
-            </div>\
-            <div class="title">Panel Page 1</div>\
-          </div>\
-        </div>\
-        <div class="page-content">\
-          <div class="block">\
-            <p>This is a right panel page 1</p>\
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo saepe aspernatur inventore dolorum voluptates consequatur tempore ipsum! Quia, incidunt, aliquam sit veritatis nisi aliquid porro similique ipsa mollitia eaque ex!</p>\
-          </div>\
-        </div>\
-      </div>\
-    ',
-  },
-  {
-    path: '/panel-right-2/',
-    content: '\
-      <div class="page">\
-        <div class="navbar">\
-          <div class="navbar-inner sliding">\
-            <div class="left">\
-              <a href="#" class="link back">\
-                <i class="icon icon-back"></i>\
-                <span class="ios-only">Back</span>\
-              </a>\
-            </div>\
-            <div class="title">Panel Page 2</div>\
-          </div>\
-        </div>\
-        <div class="page-content">\
-          <div class="block">\
-            <p>This is a right panel page 2</p>\
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo saepe aspernatur inventore dolorum voluptates consequatur tempore ipsum! Quia, incidunt, aliquam sit veritatis nisi aliquid porro similique ipsa mollitia eaque ex!</p>\
-          </div>\
-        </div>\
-      </div>\
-    ',
-  },
 
   // Components
   {
